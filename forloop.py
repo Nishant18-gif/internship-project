5fe195063b61e02053b1ad41ff6d51a38ab1acd5
Nishant18-gif <nishanrpareek89@gmail.com>
2026-02-13 10:45:01 +0530

    added for loop program

---
+++ forloop.py
@@ -0,0 +1,14 @@
+#str="apnacollege"
+#for val in str:
+#    print(val,end="")
+
+
+str="apnacollege"
+for char in str:
+    if(char=='o'):
+        print("char found o  ")
+        break
+    print(char)
+else:
+ print("end")    
+        
